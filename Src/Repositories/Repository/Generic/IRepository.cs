36526fca7320c9601c8928c77a6872e51a0bed2a
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2022-12-02 19:26:08 -0300

    Repositório genérico implementado.

---
+++ Src/Repositories/Repository/Generic/IRepository.cs
@@ -0,0 +1,13 @@
+using System.Linq.Expressions;
+
+namespace Src.Repositories.Repository.Generic
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> Get();
+        T GetById(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
