17b28e97cc141483b452d17de42ccf112de69331
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2022-12-06 21:09:16 -0300

    Cors atualizado

--- Src/Program.cs
+++ Src/Program.cs
@@ -27,7 +27,7 @@ if (app.Environment.IsDevelopment())
 
 // app.UseHttpsRedirection();
 
-app.UseCors(options => options.AllowAnyOrigin());
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseAuthorization();
 
